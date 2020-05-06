dcafa0ac7e3e63eafdc769b65de8ee23db9d5290
Aziz Ben Salem <azizbensalem1998@gmail.com>
2020-05-06 13:58:14 +0100

    Updates

---
+++ precache-manifest.9ff402505849b0093a4456af3862d116.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbad45356bd6c7701866d986678182a",
+    "url": "/clicnfest/index.html"
+  },
+  {
+    "revision": "c7914e40d7c23bd128db",
+    "url": "/clicnfest/static/css/main.4aa02b2e.chunk.css"
+  },
+  {
+    "revision": "2d33f277031fec412606",
+    "url": "/clicnfest/static/js/2.f90b6b3a.chunk.js"
+  },
+  {
+    "revision": "9d2f45ea5a3a2882282cd7f2c1137727",
+    "url": "/clicnfest/static/js/2.f90b6b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7914e40d7c23bd128db",
+    "url": "/clicnfest/static/js/main.2cb4680e.chunk.js"
+  },
+  {
+    "revision": "95e851fc650cb47d0744",
+    "url": "/clicnfest/static/js/runtime-main.2257920b.js"
+  },
+  {
+    "revision": "d8c8d1e43840ea9de70efd318fde9840",
+    "url": "/clicnfest/static/media/clicnfest.d8c8d1e4.PNG"
+  },
+  {
+    "revision": "f2ee4276f419c82b1e7a054e0723544c",
+    "url": "/clicnfest/static/media/clicnfestblue.f2ee4276.png"
+  },
+  {
+    "revision": "e0590bbae066b1562106bd2db770f78c",
+    "url": "/clicnfest/static/media/eventu.e0590bba.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicnfest/precache-manifest.c9fd9b9b82824ef249abfb327895eca7.js"
+  "/clicnfest/precache-manifest.9ff402505849b0093a4456af3862d116.js"
 );
 
 self.addEventListener('message', (event) => {
