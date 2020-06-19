49e73b62fec63f807455e280d3770889a881f996
Aziz Ben Salem <azizbensalem1998@gmail.com>
2020-06-19 17:43:18 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicnfest/precache-manifest.3dc59da420acd137f7b99fef9843bfe6.js"
+  "/clicnfest/precache-manifest.9723e1e82b4b33d40db0cdd2feecb33a.js"
 );
 
 self.addEventListener('message', (event) => {
